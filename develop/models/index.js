cf7359d075d0127449d66c9e9749b360c74a2a91
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-20 23:25:59 +0000

    associations added to models/index.js

--- develop/models/index.js
+++ develop/models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+// The association can also be created from the Car side
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: 'tag_id'
+})
+
 
 module.exports = {
   Product,
