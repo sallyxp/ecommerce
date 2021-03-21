58df174bf794ba5421d5f5f9b20c83ae541fd08e
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-21 14:08:29 +0000

    category-routes added

--- develop/routes/api/category-routes.js
+++ develop/routes/api/category-routes.js
@@ -7,72 +7,84 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
 
- /// Category.findAll().then((bookData) => {
- ///  res.json(bookData);
- /// });
 
-});
+    try {
+      const CategoryData = await Category.findAll();
+      res.status(200).json(CategoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  
-  /// Find a single book by its primary key (book_id)
- /// Book.findByPk(req.params.id).then((bookData) => {
- ///   res.json(bookData);
- // });
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with locations, using the Trip through table
+      include: [{ model: Product }]
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+//Create a category
 router.post('/', (req, res) => {
   // create a new category
- /// Book.create(req.body)
- /// .then((newBook) => {
- ///   res.json(newBook);
- /// })
- /// .catch((err) => {
- ///   res.json(err);
- /// });
-});
+ 
+    try {
+      const CategoryData = await Category.create(req.body);
+      res.status(200).json(CategoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
+//Update a category  
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-   // // Calls the update method on the Book model
-    Book.update(
-      {
-        // All the fields you can update and the data attached to the request body.
-        title: req.body.title,
-        author: req.body.author,
-        isbn: req.body.isbn,
-        pages: req.body.pages,
-        edition: req.body.edition,
-        is_paperback: req.body.is_paperback,
-      },
-      {
-        // Gets the books based on the isbn given in the request parameters
-        where: {
-          isbn: req.params.isbn,
-        },
-      }
-    )
-      .then((updatedBook) => {
-        // Sends the updated book as a json response
-        res.json(updatedBook);
-      })
-      .catch((err) => res.json(err))
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(CategoryData => {
+    if(!CategoryData[0]) {
+      res.status(404).json({ message: 'No category found with this id.' });
+      return; 
+    }
+    res.json(CategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+//Delete a category
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-   // Looks for the books based on isbn given in the request parameters and deletes the instance from the database
-   Book.destroy({
-    where: {
-      isbn: req.params.isbn,
-    },
-  })
-    .then((deletedBook) => {
-      res.json(deletedBook);
-    })
-    .catch((err) => res.json(err));
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        category_id: req.params.id
+      }
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
